b58c35f80f90242643922c179ab1e1c0b0e7a32d
Kevin Allen <allenkevinc@gmail.com>
2026-09-16 10:09:34 -0700

    1

--- firebase-init.js
+++ firebase-init.js
@@ -2,7 +2,7 @@ import { initializeApp } from './vendor/firebase/12.18.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from './vendor/firebase/12.18.0/firebase-auth.js';
 import { getFirestore, doc, collection, getDoc, getDocs, onSnapshot, runTransaction, setDoc, deleteDoc } from './vendor/firebase/12.18.0/firebase-firestore.js';
 
-window.CreamCornFirebase = {
+window.EstuaryFirebase = {
   create: config => {
     const app = initializeApp(config);
     return {
